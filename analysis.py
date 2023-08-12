7ac24a0743dc0e428ef60865c8a15e4074134040
Kabilan Prasanna <kabilanprasanna@gmail.com>
2023-08-12 17:11:00 -0400

    adding more levenshtein distance

--- analysis.py
+++ analysis.py
@@ -1,45 +1,90 @@
 # analyses
 
-from plotnine import ggplot, aes, geom_histogram
+from plotnine import ggplot, ggtitle, aes, geom_histogram
 from levenshteindist import lev2
 import pandas as pd
 import csv
 
 # LEVENSHTEIN DISTANCE >>>
 
-dists = []
-normalized_dists = []
 sents = []
 
-with open("ipadataset.csv") as f:
+dists_aa = []
+normalized_dists_aa = []
+
+dists_a1 = []
+normalized_dists_a1 = []
+
+dists_a2 = []
+normalized_dists_a2 = []
+
+with open("regdataset.csv") as f:
 
     reader = csv.DictReader(f)
     
     for row in reader:
 
-        sents.append(row["transliterated"])
+        transliterated = row["transliterated"]
         annotator1 = row["colloquial: annotator 1"]
         annotator2 = row["colloquial: annotator 2"]
 
-        dist = lev2(annotator1, annotator2)
-        normalized = dist/max(len(annotator1), len(annotator2))
 
-        dists.append(dist)
-        normalized_dists.append(normalized)
+        # get regular and normalized levenshtein distances between each pair
+        dist_aa = lev2(annotator1, annotator2)
+        normalized_aa = dist_aa/max(len(annotator1), len(annotator2))
 
+        dist_a1 = lev2(transliterated, annotator1)
+        normalized_a1 = dist_aa/max(len(transliterated), len(annotator1))
 
-d = {"original" : sents, "dists" : dists, "normalized dists" : normalized_dists}
-dist_dataframe = pd.DataFrame(data=d)
-
-
-print("max dist: " + str(max(dists)))
-print(f"average dist {sum(dists)/len(dists)}\n")
-
-print(f"max normalized dist: {max(normalized_dists)}")
-print(f"average normalized dists {sum(normalized_dists)/len(normalized_dists)}\n")
-
-print(dist_dataframe.loc[dist_dataframe["normalized dists"].idxmax()])
+        dist_a2 = lev2(transliterated, annotator2)
+        normalized_a2 = dist_aa/max(len(transliterated), len(annotator2))
 
 
-plot = ggplot(dist_dataframe, aes(x="normalized dists")) + geom_histogram(bins=100)
+        # append distances to lists
+        sents.append(transliterated)
+
+        dists_aa.append(dist_aa)
+        normalized_dists_aa.append(normalized_aa)
+
+        dists_a1.append(dist_a1)
+        normalized_dists_a1.append(normalized_a1)
+
+        dists_a2.append(dist_a2)
+        normalized_dists_a2.append(normalized_a2)
+
+
+d_aa = {"original" : sents, "dists" : dists_aa, "normalized dists" : normalized_dists_aa}
+df_aa = pd.DataFrame(data=d_aa)
+
+d_a1 = {"original" : sents, "dists" : dists_a1, "normalized dists" : normalized_dists_a1}
+df_a1 = pd.DataFrame(data=d_a1)
+
+d_a2 = {"original" : sents, "dists" : dists_a2, "normalized dists" : normalized_dists_a2}
+df_a2 = pd.DataFrame(data=d_a2)
+
+
+print("max dist: " + str(max(dists_aa)))
+print(f"max normalized dist: {max(normalized_dists_aa)}")
+print(df_aa.loc[df_aa["normalized dists"].idxmax()])
+
+print("\nbetween two annotators:\n-----------")
+print(f"average dist {sum(dists_aa)/len(dists_aa)}")
+print(f"average normalized dists {sum(normalized_dists_aa)/len(normalized_dists_aa)}\n\n")
+
+print("between transliteration and annotator 1:\n-----------")
+print(f"average dist {sum(dists_a1)/len(dists_a1)}")
+print(f"average normalized dists {sum(normalized_dists_a1)/len(normalized_dists_a1)}\n\n")
+
+print("between transliteration and annotator 2:\n-----------")
+print(f"average dist {sum(dists_a2)/len(dists_a2)}")
+print(f"average normalized dists {sum(normalized_dists_a2)/len(normalized_dists_a2)}\n\n")
+
+
+plot = ggplot(df_aa, aes(x="normalized dists")) + geom_histogram(bins=100) + ggtitle("between two annotators")
 print(plot)
+
+plot1 = ggplot(df_a1, aes(x="normalized dists")) + geom_histogram(bins=100) + ggtitle("between transliteration + annotator 1")
+print(plot1)
+
+plot2 = ggplot(df_a2, aes(x="normalized dists")) + geom_histogram(bins=100) + ggtitle("between transliteration + annotator 2")
+print(plot2)
