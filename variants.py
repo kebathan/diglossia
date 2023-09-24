3d4731f955a49fda3fcdaab70d3fa955c3e68ca5
Kabilan Prasanna <kabilanprasanna@gmail.com>
2023-09-24 16:57:41 -0400

    changing orthography

--- variants.py
+++ variants.py
@@ -38,7 +38,66 @@ def accusative(text):
     return " ".join(text)
 
 
-ex = "ivargal palliyil puthangangalai padippaargal"
+def gemination(text, geminate):
+    
+    for gem in geminate:
+        while text.lower().find(gem) != -1:
+            ungem = gem[:int(len(gem)/2)]
+            text = text.replace(gem, ungem)
 
-print(ex)
-print(accusative(locative(plural(ex))))
+    return text
+
+
+def vowel_orth(text, vowels):
+
+    vows = {"ee" : "ii",
+            "oo" : "uu",
+            "aa" : "a"}
+    
+    for vow in vowels:
+        while text.lower().find(vow) != -1:
+            text = text.replace(vow, vows[vow])
+
+    return text
+
+
+def h_g(text):
+
+    vowels = ("a", "e", "i", "o", "u")
+    text = text.lower()
+    idx = 0
+
+    while text[idx+1:].find("h") != -1:
+
+        idx = text[idx:].find("h")
+        
+        if idx > 0 and idx < len(text) - 1:
+            if text[idx-1] in vowels and text[idx+1] in vowels:
+                text = text.replace("h", "g")
+
+    return text
+
+
+def ch_s(text):
+
+    text = text.lower().split()
+
+    for i in range(len(text)):
+        if text[i].find("ch") == 0:
+            text[i] = text[i].replace("ch", "s")
+
+    return " ".join(text)
+
+
+def orth_replace(text, rep):
+
+    while text.lower().find(rep[0]) != -1:
+        text = text.replace(rep[0], rep[1])
+
+    return text
+
+
+test = "changar mattrum ivargal taj mahalil thamizh puththagangalai padippaargal"
+
+print(test)
+print(h_g(ch_s(orth_replace(orth_replace(gemination(accusative(locative(plural(test))), ["thth", "pp"]), ("zh", "l")), ("th", "t")))))
